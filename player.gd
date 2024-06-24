2172724220a513d83746d42cf467c210c96c2937
Minkampel <kontakt@goldperbrief.de>
2024-06-24 13:51:18 +0200

    asd

--- player.gd
+++ player.gd
@@ -2,17 +2,22 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMPS = 2
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var num_jumps = 0
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		num_jumps = 0
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and ( is_on_floor() or num_jumps < MAX_JUMPS ):
 		velocity.y = JUMP_VELOCITY
+		num_jumps += 1
 		
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
