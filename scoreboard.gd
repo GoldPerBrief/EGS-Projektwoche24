d82ef28d2d5208deda2194444c3c1cc64fac0dc6
Minkampel <emil@grasskamp.de>
2024-06-24 22:13:25 +0200

    Commit from 2024-06-24_22:13:25

--- scoreboard.gd
+++ scoreboard.gd
@@ -14,10 +14,8 @@ func read_scores_from_csv():
 	var scores = []
 	
 	var file = FileAccess.open("user://scores.csv", FileAccess.READ)
-
 	var header = file.get_line()
 	
-	# Read each subsequent line (which contains name,score)
 	while !file.eof_reached():
 		var line = file.get_line()
 		if line != "":
@@ -25,17 +23,24 @@ func read_scores_from_csv():
 			if parts.size() == 2:
 				var name = parts[0]
 				var score = parts[1].to_int()
-				scores.append({"name": name, "score": score})
-		
-		file.close()
+				scores.append([score, name])
+
+	scores.sort_custom(func(a, b): return a[0] > b[0])
+	file.close()
 		
 	return scores
 
+
+
 func format_scores(scores):
-	var formatted_text = "Hign Scores:\n\n"
-	
+	var formatted_text = "High Scores:\n\n"
+	var i = 0
 	for score_data in scores:
-		formatted_text += "{0}: {1}\n".format(score_data["name"], score_data["score"])
+		i += 1
+		formatted_text += "{0}: {1}\n".format([score_data[1], score_data[0]])
+		print(score_data)
+		if i > 6:
+			break
 	return formatted_text
 
 func sb_update():
