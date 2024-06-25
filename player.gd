4ef374f5bf5d2b1cc12b70b51b875589fdc9738e
Minkampel <kontakt@goldperbrief.de>
2024-06-25 09:38:51 +0200

    asdasdasd

--- player.gd
+++ player.gd
@@ -11,12 +11,8 @@ var score = 0
 var highscore = 0
 var username = ""
 
-@onready var text_input = $"../CanvasLayer/TextEdit"
-@onready var save_button = $"../CanvasLayer/Save_Button"
-@onready var discard_button = $"../CanvasLayer/Discard_Button"
-@onready var message_label = $"../CanvasLayer/Label"
-@onready var score_label = $"../CanvasLayer/RichTextLabel"
 @onready var scoreboard = $"../CanvasLayer/Scoreboard"
+@onready var score_label = $"../CanvasLayer/RichTextLabel"
 @onready var area_collider = $Area2D
 
 func _ready():
@@ -24,8 +20,7 @@ func _ready():
 	update_score_text()
 	
 	#Connect button signals
-	save_button.connect("pressed", Callable(self, "_on_save_pressed"))
-	discard_button.connect("pressed", Callable(self, "_on_discard_pressed"))
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -50,44 +45,6 @@ func _physics_process(delta: float) -> void:
 	get_parent().get_node("Camera2D").position = position
 	move_and_slide()
 
-func _on_save_pressed():
-	var user_text = text_input.text.strip_edges()
-	if user_text.is_empty():
-		message_label.text = "Bitte Namen eingeben!"
-		return
-	
-	var bad_phrases = load_bad_phrases()
-	
-	if contains_bad_phrase(user_text, bad_phrases):
-		message_label.text = "Bitte Eingabe revisieren, da diese verbotene Worte enthält!"
-		return
-		
-	username = user_text
-	
-	text_input.text = ""
-	message_label.text = "Punktestand gesichtert!"
-	
-func _on_discard_pressed():
-	text_input.text = ""
-	message_label.text = "Punktestand nicht gesichert!"
-	
-func load_bad_phrases() -> Array:
-	var bad_phrases = []
-	var file = FileAccess.open(BAD_PHRASES_FILE, FileAccess.READ)
-	if OK:
-		while !file.eof_reached():
-			var line = file.get_line().strip_edges()
-			if not line.is_empty():
-				bad_phrases.append(line)
-		file.close()
-		
-	return bad_phrases
-	
-func contains_bad_phrase(text: String, bad_phrases: Array) -> bool:
-	for phrase in bad_phrases:
-		if text.find(phrase, 0) != -1:
-			return true
-	return false
 
 func make_dead():
 	queue_free()
@@ -97,9 +54,8 @@ func make_dead():
 	else:
 		print("u didnt even get any points. u sux.")
 	pass
-	update_highscore_in_csv(score)
-	scoreboard.sb_update()
-	scoreboard.sb_show()
+	scoreboard.set_visibility_for_all(true)
+	scoreboard.set_score(score)
 
 func collect_stuff(points_amount: int):
 	score += points_amount
@@ -135,10 +91,5 @@ func get_highest_score() -> int:
 func update_score_text():
 	score_label.text = "[b]Highscore:\t%d\nPunkte:\t\t%d\n[/b]" % [highscore, score]
 	
-func update_highscore_in_csv(new_highscore: int):
-	var file = FileAccess.open("user://scores.csv", FileAccess.READ_WRITE)
-	while not file.eof_reached():
-		file.get_csv_line()
-	file.store_csv_line(["John Doe", new_highscore])
-	
+
 	
--- scoreboard.gd
+++ scoreboard.gd
@@ -1,14 +1,30 @@
 extends RichTextLabel
 
+@onready var scoreboard = $"../Scoreboard"
+@onready var text_input = $"../TextEdit"
+@onready var save_button = $"../Save_Button"
+@onready var discard_button = $"../Discard_Button"
+@onready var message_label = $"../Label"
+
+var username = ""
+var score = 0
+
+const BAD_PHRASES_FILE = "res://bad_phrases.txt"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	save_button.connect("pressed", Callable(self, "_on_save_pressed"))
+	discard_button.connect("pressed", Callable(self, "_on_discard_pressed"))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func set_score(new_score: int):
+	score = new_score
 	
 func read_scores_from_csv():
 	var scores = []
@@ -54,3 +70,63 @@ func sb_show():
 	
 func sb_hide():
 	$".".visible = false
+
+func _on_save_pressed():
+	var user_text = text_input.text.strip_edges()
+	if user_text.is_empty():
+		message_label.text = "Bitte Namen eingeben!"
+		return
+	
+	#var bad_phrases = load_bad_phrases()
+	
+	#if contains_bad_phrase(user_text, bad_phrases):
+		#message_label.text = "Bitte Eingabe revisieren, da diese verbotene Worte enthält!"
+		#return
+		
+	username = user_text
+	
+	text_input.text = ""
+	message_label.text = "Punktestand gesichtert!"
+	update_highscore_in_csv(score)
+	scoreboard.sb_update()
+	set_visibility_for_all(false)
+	scoreboard.sb_show()
+	
+func _on_discard_pressed():
+	text_input.text = ""
+	message_label.text = "Punktestand nicht gesichert!"
+	scoreboard.sb_update()
+	set_visibility_for_all(false)
+	scoreboard.sb_show()
+	
+func load_bad_phrases() -> Array:
+	var bad_phrases = []
+	var file = FileAccess.open(BAD_PHRASES_FILE, FileAccess.READ)
+	if OK:
+		while !file.eof_reached():
+			var line = file.get_line().strip_edges()
+			if not line.is_empty():
+				bad_phrases.append(line)
+		file.close()
+		
+	return bad_phrases
+	
+func contains_bad_phrase(text: String, bad_phrases: Array) -> bool:
+	for phrase in bad_phrases:
+		if text.find(phrase, 0) != -1:
+			return true
+	return false
+	
+func update_highscore_in_csv(new_highscore: int):
+	var file = FileAccess.open("user://scores.csv", FileAccess.READ_WRITE)
+	while not file.eof_reached():
+		file.get_csv_line()
+	file.store_csv_line([username, new_highscore])
+	
+
+func set_visibility_for_all(is_visible: bool):
+	text_input.visible = is_visible
+	save_button.visible = is_visible
+	discard_button.visible = is_visible
+	message_label.visible = is_visible
+	
